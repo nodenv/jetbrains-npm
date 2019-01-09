48f45519308512cc07065bfdb3bd590f7cd2c7c7
Jason Karns <jason@karns.name>
2019-01-09 10:40:03 -0500

    Rewrite shim to be more robust
    Can be installed as nodenv plugin, npm package, or cloned directly to
    nodenv root as lib/node_modules/npm.
    
    Each these approaches requires different setup.

--- bin/npm-cli.js
+++ bin/npm-cli.js
@@ -1,6 +1,29 @@
 #!/usr/bin/env node
 
-var nodenvRoot = process.env.NODENV_ROOT || process.env.HOME + '/.nodenv'
-require('child_process')
-  .spawn(nodenvRoot + '/shims/npm', process.argv.slice(2), {env: process.env, stdio: 'inherit'})
-  .on('close', process.exit) // warning: npm <4 doesn't preserve exit code
+var child_process = require('child_process')
+var fs = require('fs')
+var path = require('path')
+
+child_process.exec('nodenv root', function(err, stdout, stderr) {
+  var npm
+
+  // get shim path from `nodenv root` (respects $NODENV_ROOT)
+  if(!err && stdout) {
+    npm = npmShim(stdout.replace(/\n/g, ''))
+  }
+
+  // infer shim path if installed as nodenv plugin
+  if (!npm && path.basename(path.resolve(__dirname + '/../..')) === 'plugins') {
+    npm = npmShim(path.resolve(__dirname + '/../../..'))
+  }
+
+  // fallback to npm from PATH if not found thus far
+  child_process
+    .spawn(npm || 'npm', process.argv.slice(2), { stdio: 'inherit' })
+    .on('close', process.exit)
+})
+
+function npmShim(nodenvRoot) {
+  var npmShim = nodenvRoot + '/shims/npm'
+  if (fs.existsSync(npmShim)) return npmShim
+}
